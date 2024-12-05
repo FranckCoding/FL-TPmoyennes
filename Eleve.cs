364a099edbbd63ef899fec4d0b1cab82c8f6664c
FranckCoding <leger_franck@protonmail.com>
2024-12-05 12:51:01 +0100

    Ajout des attributs de classe au modèle Eleve

--- Eleve.cs
+++ Eleve.cs
@@ -3,10 +3,23 @@
 namespace HNI_TPmoyennes
 {
 	/// <summary>
-	/// Summary description for Class1
+	/// Décrit les informations d'un élève présent dans une classe
 	/// </summary>
 	public class Eleve
 	{
+		/// <summary>
+		/// Prénom de l'élève
+		/// </summary>
+		public String prenom {  get; set; }
+		/// <summary>
+		/// Nom de famille de l'élève
+		/// </summary>
+		public String nom { get; set; }
+		/// <summary>
+		/// Liste des notes de l'élèves
+		/// </summary>
+		public List<Note> notes { get; set; }
+
 		public Eleve()
 		{
 			//
