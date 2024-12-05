a0c4bdbf104cece4d02cd071d210ca839aee9dd1
FranckCoding <leger_franck@protonmail.com>
2024-12-05 12:47:23 +0100

    Creation du modèle Eleve

---
+++ Eleve.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HNI_TPmoyennes
+{
+	/// <summary>
+	/// Summary description for Class1
+	/// </summary>
+	public class Eleve
+	{
+		public Eleve()
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
+	}
+}
