70f40ec92968e8b7ee3d25c94d064e1fd69c3b0e
FranckCoding <leger_franck@protonmail.com>
2024-12-05 10:56:22 +0100

    Creation du modele Classe avec les attributs et constructeur

---
+++ Classe.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace HNI_TPmoyennes
+{
+	/// <summary>
+	/// Description du modèle d'une Classe
+	/// contenant un nom, la liste des élèves, des matières et la moyenne générale de la classe
+	/// </summary>
+	public class Classe
+	{
+		/// <summary>
+		/// Nom de la classe
+		/// </summary>
+		public String nom {  get; set; }
+
+		/// <summary>
+		/// Liste des élèves dans la classe
+		/// </summary>
+        public List<Eleve> listEleve { get; set; }
+
+        /// <summary>
+        /// Liste des matières enseignées dans la classe avec les 
+        /// moyennes associées
+        /// </summary>
+        public Dictionary<string, int> listMatiere { get; set; }
+
+        /// <summary>
+        /// Moyenne générale de la classe
+        /// </summary>
+        public int moyenneGeneral { get; set; }
+
+		/// <summary>
+		/// Constructeur du modèle Classe
+		/// </summary>
+		/// <param name="nom">Nom de la classe à attribuer</param>
+        public Classe(String nom)
+		{
+            this.nom = nom;
+		}
+    }
+}
